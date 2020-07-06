cffd7fd28153cd4f8ab1b593e73766c7b61e29d9
B0218953 <b0218953@B0218953s-MacBook-Pro.local>
2020-07-07 00:04:20 +0530

    Updates

---
+++ precache-manifest.68427a03bcddbe808702ba218da31f3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475ea85f2b6d4f26db9c3b753e7f776b",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "7b387fd55161b89d062c",
+    "url": "/react-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c63ff3aa90d2710edc06",
+    "url": "/react-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b387fd55161b89d062c",
+    "url": "/react-test/static/js/2.107e5de0.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/react-test/static/js/2.107e5de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63ff3aa90d2710edc06",
+    "url": "/react-test/static/js/main.2ef314a5.chunk.js"
+  },
+  {
+    "revision": "14d041c37aa1e787e0b2",
+    "url": "/react-test/static/js/runtime-main.08fd1e44.js"
+  }
+]);
